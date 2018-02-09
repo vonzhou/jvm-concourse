6635b938830a61b20c0157913376aa1e3da2e58d
vonzhou <vonzhou@163.com>
2018-02-09 17:04:03 +0800

    PretenureSizeThreshold大对象直接进入老年代

--- src/main/java/com/vonzhou/learn/jvm/TestAllocation.java
+++ src/main/java/com/vonzhou/learn/jvm/TestAllocation.java
@@ -9,14 +9,12 @@ public class TestAllocation {
     private static final int _1MB = 1024 * 1024;
 
     public static void main(String[] args) {
-        testAllocation();
+//        testAllocation();
+        testPretenureSizeThrehold();
     }
 
-
-
-
     /**
-     * -XX:UseSerialGC -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+UseSerialGC  -XX:+PrintGCDetails -XX:SurvivorRatio=8
+     * -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+UseSerialGC  -XX:+PrintGCDetails -XX:SurvivorRatio=8
      *
      *
      [GC (Allocation Failure) [DefNew: 7784K->631K(9216K), 0.0031818 secs] 7784K->6775K(19456K), 0.0032193 secs] [Times: user=0.00 sys=0.00, real=0.00 secs]
@@ -29,19 +27,19 @@ public class TestAllocation {
      the space 10240K,  60% used [0x00000000ff600000, 0x00000000ffc00030, 0x00000000ffc00200, 0x0000000100000000)
      Metaspace       used 3037K, capacity 4494K, committed 4864K, reserved 1056768K
      class space    used 332K, capacity 386K, committed 512K, reserved 1048576K
-
+    
      * 内存布局：
      * ------------------------------------------------------------------
      * from | to |     eden                |          old               |
      * ------------------------------------------------------------------
      * 1M     1M         8M                       10M
-
+    
      a1 = new byte[2 * _1MB];  // from survivor 不够，进入 Eden
      a2 = new byte[2 * _1MB]; // 进入 Eden
      a3 = new byte[2 * _1MB];  // 进入 Eden
      a4 = new byte[4 * _1MB];  // 年轻代需要GC， Minor GC，之前的6MB进入 old，a4分配到Eden。
-
-
+    
+    
      *
      *
      *
@@ -67,5 +65,23 @@ public class TestAllocation {
         a3 = new byte[2 * _1MB];
         a4 = new byte[4 * _1MB];
 
+    }
+
+    /**
+     * -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+UseSerialGC  -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:PretenureSizeThreshold=3M
+     *
+     Heap
+     def new generation   total 9216K, used 1804K [0x00000000fec00000, 0x00000000ff600000, 0x00000000ff600000)
+     eden space 8192K,  22% used [0x00000000fec00000, 0x00000000fedc31e8, 0x00000000ff400000)
+     from space 1024K,   0% used [0x00000000ff400000, 0x00000000ff400000, 0x00000000ff500000)
+     to   space 1024K,   0% used [0x00000000ff500000, 0x00000000ff500000, 0x00000000ff600000)
+     tenured generation   total 10240K, used 4096K [0x00000000ff600000, 0x0000000100000000, 0x0000000100000000)   // 直接在老年代
+     the space 10240K,  40% used [0x00000000ff600000, 0x00000000ffa00010, 0x00000000ffa00200, 0x0000000100000000)
+     Metaspace       used 3171K, capacity 4494K, committed 4864K, reserved 1056768K
+     class space    used 348K, capacity 386K, committed 512K, reserved 1048576K
+    
+     */
+    public static void testPretenureSizeThrehold() {
+        byte[] a = new byte[4 * _1MB];
     }
 }
