bb7799d35d6749d18276ab59f6710f3f32bd7d7d
vonzhou <vonzhou@163.com>
2018-02-12 14:46:26 +0800

    字节码格式分析

---
+++ src/main/java/com/vonzhou/learn/jvm/ch07class/TestClass.java
@@ -0,0 +1,12 @@
+package com.vonzhou.learn.jvm.ch07class;
+
+/**
+ * @version 2018/2/12.
+ */
+public class TestClass {
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+}
---
+++ src/main/java/com/vonzhou/learn/jvm/ch07class/TestClass3.java
@@ -0,0 +1,14 @@
+package com.vonzhou.learn.jvm.ch07class;
+
+/**
+
+ *
+ * @version 2018/2/12.
+ */
+public class TestClass3 {
+    private final static int m = 123;
+
+    public static int inc() {
+        return m + 1;
+    }
+}
