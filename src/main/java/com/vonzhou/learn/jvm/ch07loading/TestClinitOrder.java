0227965a21667991b8cab5be916930247eecef00
vonzhou <vonzhou@163.com>
2018-02-21 16:49:33 +0800

    父类的<clinit>方法会先执行

---
+++ src/main/java/com/vonzhou/learn/jvm/ch07loading/TestClinitOrder.java
@@ -0,0 +1,23 @@
+package com.vonzhou.learn.jvm.ch07loading;
+
+/**
+ * P226
+ * Created by vonzhou on 2018/2/21.
+ */
+public class TestClinitOrder {
+    static class Parent {
+        public static int A = 1;
+
+        static {
+            A = 2;
+        }
+    }
+
+    static class Sub extends Parent {
+        public static int B = A;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Sub.B);
+    }
+}
