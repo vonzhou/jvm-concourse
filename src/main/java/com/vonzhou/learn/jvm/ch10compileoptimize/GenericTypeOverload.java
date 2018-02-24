546108ba3421cadb43597cd119e70fd434733783
vonzhou <vonzhou@163.com>
2018-02-24 14:30:09 +0800

    当泛型遇到重载

---
+++ src/main/java/com/vonzhou/learn/jvm/ch10compileoptimize/GenericTypeOverload.java
@@ -0,0 +1,22 @@
+package com.vonzhou.learn.jvm.ch10compileoptimize;
+
+import java.util.List;
+
+/**
+ * 当泛型遇到重载
+ * @version 2018/2/24.
+ */
+public class GenericTypeOverload {
+    // 'method(List<String>)' clashes with 'method(List<Integer>)'; both methods have same erasure
+//    public static void method(List<String> list) {
+//    }
+
+    public static String method(List<String> list) {
+        return "";
+    }
+
+    public static int method(List<Integer> list) {
+        return 1;
+    }
+
+}
